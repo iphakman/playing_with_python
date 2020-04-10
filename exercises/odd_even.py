158a32abe1e1aeb146cb6c88a02a2ea0ad652064
iphakman <32890983+iphakman@users.noreply.github.com>
2020-04-10 15:24:53 -0500

    Exersices odd or even

---
+++ exercises/odd_even.py
@@ -0,0 +1,10 @@
+"""
+simplemente decir si el numero es par o impar
+"""
+
+mi_num = int(input("Enter a number: "))
+
+if mi_num % 2 == 0:
+    print(mi_num, "es un numero par!")
+else:
+    print(mi_num, "es un numero inpar!")
