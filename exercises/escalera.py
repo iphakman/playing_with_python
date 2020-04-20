a9011e15dbd2983550e846f9f761181b8711b04f
Joshue <32890983+iphakman@users.noreply.github.com>
2020-04-20 14:12:38 -0500

    Ejercicio escalera

---
+++ exercises/escalera.py
@@ -0,0 +1,7 @@
+"""
+Teniendo una escalera de N peldaños, puedes subir unicamente de 1 o 2 a la vez.
+
+Escribe un algoritmo, que nos diga el numero de combinaciones posibles en las que se podria subir la escalera.
+"""
+
+
